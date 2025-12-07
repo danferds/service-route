911779fa36e4e8d6e2f73513525b0c6f1fbb7820
Daniel Fernandes <danielferdsc@gmail.com>
2025-12-06 21:39:45 -0300

    chore: configuração inicial.

---
+++ serviceroute/src/main/java/com/sr/serviceroute/ServiceRouteApplication.java
@@ -0,0 +1,13 @@
+package com.sr.serviceroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRouteApplication.class, args);
+	}
+
+}
---
+++ serviceroute/src/test/java/com/sr/serviceroute/ServiceRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sr.serviceroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
